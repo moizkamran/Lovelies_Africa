72cc07c0b1aebb7dc36c501b618390ff0cf3f4b0
NakanaTra <nakana.traore@live.fr>
2023-04-22 06:37:27 +0200

    mise à jour 8

--- client/src/MainPage/HomePage.js
+++ client/src/MainPage/HomePage.js
@@ -13,6 +13,28 @@ const Home = () => {
       </section>
 
       {/* carroussel */}
+      <div id="carouselExampleIndicators" className="carousel">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="../img/slider/slider1.jpg" className="d-block w-100 carroussel" alt="déco salon éventail"/>
+    </div>
+    <div className="carousel-item">
+      <img src="../img/slider/slider2.jpg" className="d-block w-100 carroussel" alt="tissus wax"/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
 
       <section className="description">
         <article className="article">
